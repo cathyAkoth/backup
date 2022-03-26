17e6f9227b63054cec37eb58a1921067faef2a13
Akoth Cathy <catherinececilia22@gmail.com>
2022-03-26 23:02:50 +0300

    Added agentRoute

---
+++ routes/agentRoute.js
@@ -0,0 +1,62 @@
+const router  = require('express').Router()
+const AgentVerificationController = require('../controllers/agentVerification.controller')
+
+
+router.get('', async(req, res) => {
+    let agentVerifications = await AgentVerificationController.fetchAgentVerifications();
+    res.json({
+        data: agentVerifications
+    })
+})
+
+router.post('/add', async (req, res) => {
+    let data = req.body;
+    let agentVerification = await AgentVerificationController.addAgentVerification(data)
+    
+    return res.status(201).json({
+        message: agentVerification
+    })
+})
+
+router.get('/:id', async (req, res) => {
+    let { id } = req.params;
+
+    let data =  await AgentVerificationController.getAgentVerification(id);
+
+    if(!data) {
+        return res.status(404).json({
+            message: "details not found"
+        })
+    }
+    return res.json({
+        data
+    })
+})
+
+router.put('/:id/update', async(req, res) => {
+    
+    let {id} = req.params
+    let agentVerification = await AgentVerificationController.getAgentVerification(id);
+    if(!agentVerification) {
+        return res.status(404).json({
+            message: "verification not found"
+        }) 
+    }
+    await AgentVerificationController(id, completed);
+    return res.json({
+        message: "agent verification  updated successfully"
+    })
+})
+
+router.delete('/:id/delete', async (req, res)=> {
+    let { id } = req.params;
+
+    let agentDetails = await AgentVerificationController.deleteAgentVerification(id);
+    return res.json({
+        agentDetails
+    })
+})
+
+
+
+module.exports = router;
