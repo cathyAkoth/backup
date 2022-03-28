32359263c302bd763c42c5b153d4d82b24d3c6c5
Akoth Cathy <catherinececilia22@gmail.com>
2022-03-28 09:15:35 +0300

    Added roles to users

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const cors =require('cors');
 require('dotenv').config()
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const path = require('path')
+const User = require('./models/userModel')
 
 // route imports
 
-const userRoutes = require('./routes/userRoutes');
+const routes = require('./routes/route.js');
 const candidateRoutes = require('./routes/candidateRoutes');
 const agentRoutes = require('./routes/agentRoutes');
 const employerIndividualRoutes = require('./routes/employerIndividualRoutes');
@@ -40,7 +43,30 @@ app.use(
 //  routes to app
 
 
-app.use('/auth', userRoutes)
+
+app.use(async (req, res, next) => {
+    if (req.headers["x-access-token"]) {
+      try {
+        const accessToken = req.headers["x-access-token"];
+        const { userId, exp } = await jwt.verify(accessToken, process.env.JWT_SECRET);
+        // If token has expired
+        if (exp < Date.now().valueOf() / 1000) {
+          return res.status(401).json({
+            error: "JWT token has expired, please login to obtain a new one"
+          });
+        }
+        res.locals.loggedInUser = await User.findById(userId);
+        next();
+      } catch (error) {
+        next(error);
+      }
+    } else {
+      next();
+    }
+  });
+  
+  app.use('/', routes);
+
 app.use('/agentVerification', agentRoutes)
 app.use('/candidateVerification', candidateRoutes)
 app.use('/employer' , employerIndividualRoutes)
